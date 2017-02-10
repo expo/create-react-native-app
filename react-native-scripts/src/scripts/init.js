914bab8827c7b6376e2577a5095f6946217817b4
Adam Perry <adam.n.perry@gmail.com>
2017-02-09 22:50:46 -0800

    Update to Exponent SDK 14 RC

--- react-native-scripts/src/scripts/init.js
+++ react-native-scripts/src/scripts/init.js
@@ -8,8 +8,7 @@ import spawn from 'cross-spawn';
 
 // UPDATE DEPENDENCY VERSIONS HERE
 const DEFAULT_DEPENDENCIES = {
-  "exponent": "~13.1.0",
-  // TODO(dikaiosune) move this to stable releases and update exponent dep
+  "exponent": "14.0.0-rc.1",
   "react": "~15.4.0",
   "react-native": "0.41.2"
 };
