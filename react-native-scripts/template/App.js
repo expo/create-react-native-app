1ca22b8ab55bdae36ea8fd7ea270c7976203c928
Adam Perry <adam.n.perry@gmail.com>
2017-03-15 15:37:46 -0700

    Tell people in the template that the dev menu is a shake gesture.

--- react-native-scripts/template/App.js
+++ react-native-scripts/template/App.js
@@ -7,6 +7,7 @@ export default class App extends React.Component {
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
       </View>
     );
   }
