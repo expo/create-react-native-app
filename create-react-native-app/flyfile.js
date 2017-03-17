5c29a0452fc748ef5192b4e2044e4422326cc743
Adam Perry <adam.n.perry@gmail.com>
2017-03-17 16:05:06 -0700

    Update fly scripts and npm scripts.

--- create-react-native-app/flyfile.js
+++ create-react-native-app/flyfile.js
@@ -11,13 +11,13 @@ export default async function (fly) {
 }
 
 export async function babel(fly, opts) {
-  await fly.source(opts.src || paths.source).babel().target(paths.build);
+  await fly.clear(paths.build).source(opts.src || paths.source).babel().target(paths.build);
 }
 
 export async function clean(fly) {
   await fly.clear(paths.build);
 }
 
-export async function build(fly) {
-  await fly.start('babel');
+export async function publish(fly) {
+  await fly.serial(["clean", "babel"]).shell("npm publish");
 }
--- react-native-scripts/flyfile.js
+++ react-native-scripts/flyfile.js
@@ -6,22 +6,18 @@ const paths = {
   sourceRoot: join(__dirname, 'src'),
 };
 
+export default async function (fly) {
+  await fly.watch(paths.source, 'babel');
+}
+
 export async function babel(fly, opts) {
-  await fly.source(opts.src || paths.source).babel().target(paths.build);
+  await fly.clear(paths.build).source(opts.src || paths.source).babel().target(paths.build);
 }
 
 export async function clean(fly) {
   await fly.clear(paths.build);
 }
 
-export async function build(fly) {
-  await fly.start('babel');
-}
-
 export async function publish(fly) {
   await fly.serial(['clear', 'babel']).shell('npm publish');
-}
-
-export default async function (fly) {
-  await fly.watch(paths.source, 'babel');
 }
