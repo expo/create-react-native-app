9490fc90b41497c645a4e5beed3b6bfccca8b901
Simon Sturmer <sstur@me.com>
2017-03-22 03:27:36 +0700

    Fix unused variables in App.test.js (#105)

--- react-native-scripts/template/App.test.js
+++ react-native-scripts/template/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactNative from 'react-native';
 import App from './App';
 
 import renderer from 'react-test-renderer';
 
 it('renders without crashing', () => {
   const rendered = renderer.create(<App />).toJSON();
+  expect(rendered).toBeTruthy();
 });
