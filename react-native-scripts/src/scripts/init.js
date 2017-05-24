a47123b989a7a06150d9a48af5fbf58ad4505e8a
Brent Vatne <brentvatne@gmail.com>
2017-05-24 20:36:03 +0200

    Update dependencies in react-native-scripts for Expo SDK 17 (#224)
    - Also bump react-native-scripts to 0.0.30

--- react-native-scripts/src/scripts/init.js
+++ react-native-scripts/src/scripts/init.js
@@ -9,14 +9,14 @@ import log from '../util/log';
 
 // UPDATE DEPENDENCY VERSIONS HERE
 const DEFAULT_DEPENDENCIES = {
-  expo: '^16.0.0',
+  expo: '^17.0.0',
   react: '16.0.0-alpha.6',
-  'react-native': '^0.43.4',
+  'react-native': '^0.44.0',
 };
 
 // TODO figure out how this interacts with ejection
 const DEFAULT_DEV_DEPENDENCIES = {
-  'jest-expo': '^0.4.0',
+  'jest-expo': '~1.0.1',
   'react-test-renderer': '16.0.0-alpha.6',
 };
 
