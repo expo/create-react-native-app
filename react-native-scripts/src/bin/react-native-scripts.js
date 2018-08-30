a885b295c4d8ef5c5f6bb2c41c16ef50c9b25dbc
Ville Immonen <ville.immonen@iki.fi>
2018-08-30 18:15:03 +0300

    Add a deprecation warning and upgrade instructions.

--- react-native-scripts/src/bin/react-native-scripts.js
+++ react-native-scripts/src/bin/react-native-scripts.js
@@ -1,19 +1,45 @@
 #!/usr/bin/env node
 'use strict';
 import spawn from 'cross-spawn';
+import chalk from 'chalk';
 
 const script = process.argv[2];
 const args = process.argv.slice(3);
 
 if (script === 'start' || script === 'eject') {
+  printDeprecationMessage();
   runExpoCli(script, ...args);
 } else if (script === 'android' || script === 'ios') {
+  printDeprecationMessage();
   runExpoCli('start', `--${script}`, ...args);
 } else {
   console.log(
     `Invalid command '${script}'. Please check if you need to update react-native-scripts.`
   );
   process.exit(1);
+}
+
+function printDeprecationMessage() {
+  console.warn(chalk.bold('Note: react-native-scripts is deprecated.\n'));
+  console.warn(chalk.underline('Upgrading your project to use Expo CLI:\n'));
+  console.warn('Make these changes to package.json:');
+  console.warn(
+    chalk.bold("1) Replace 'react-native-scripts' with 'expo' in the 'scripts' config.")
+  );
+  console.warn(
+    `   Example:
+    "scripts": {
+      "start": "expo start",
+      "eject": "expo eject",
+      "android": "expo start --android",
+      "ios": "expo start --ios",
+      "test": "jest"
+    }`
+  );
+  console.warn(chalk.bold('2) Remove react-native-scripts from devDependencies.\n'));
+  console.warn(
+    chalk.bold("That's all! Expo CLI will install automatically when you run `npm start`.\n")
+  );
 }
 
 function runExpoCli(...args) {
