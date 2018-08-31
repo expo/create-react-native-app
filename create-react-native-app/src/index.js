69fa26fd7d8cfbbf2024d8a584d5e8390115a678
Ville Immonen <ville.immonen@iki.fi>
2018-08-31 19:28:15 +0300

    Delegate commands to Expo CLI (#751)
    * Delegate commands to Expo CLI
    
    * Add a deprecation warning and upgrade instructions.
    
    * Remove now unnecessary dependencies

--- create-react-native-app/src/index.js
+++ create-react-native-app/src/index.js
@@ -86,6 +86,14 @@ async function createApp(name: string, verbose: boolean, version: ?string): Prom
 
   const packageToInstall = getInstallPackage(version);
   const packageName = getPackageName(packageToInstall);
+
+  if (packageToInstall === 'react-native-scripts') {
+    // The latest version of react-native-scripts is just a wrapper for expo-cli,
+    // so we can skip installing it and just run expo-cli directly.
+    runExpoCli('init', name);
+    return;
+  }
+
   checkAppName(appName, packageName);
 
   if (!await pathExists(name)) {
@@ -274,4 +282,38 @@ async function isSafeToCreateProjectIn(root: string): Promise<boolean> {
   return (await fse.readdir(root)).every(file => {
     return validFiles.indexOf(file) >= 0;
   });
+}
+
+function runExpoCli(...args) {
+  spawn('expo-cli', args, { stdio: 'inherit' })
+    .on('exit', function(code) {
+      process.exit(code);
+    })
+    .on('error', function() {
+      console.warn('This command requires Expo CLI.');
+      var rl = require('readline').createInterface({
+        input: process.stdin,
+        output: process.stdout,
+      });
+      rl.question('Do you want to install it globally [Y/n]? ', function(answer) {
+        rl.close();
+        if (/^n/i.test(answer.trim())) {
+          process.exit(1);
+        } else {
+          console.log("Installing the package 'expo-cli'...");
+          spawn('npm', ['install', '--global', '--loglevel', 'error', 'expo-cli@latest'], {
+            stdio: ['inherit', 'ignore', 'inherit'],
+          }).on('close', function(code) {
+            if (code !== 0) {
+              console.error('Installing Expo CLI failed. You can install it manually with:');
+              console.error('  npm install --global expo-cli');
+              process.exit(code);
+            } else {
+              console.log('Expo CLI installed. You can run `expo --help` for instructions.');
+              runExpoCli(...args);
+            }
+          });
+        }
+      });
+    });
 }
