faf057d0887e627fdf59502df11adaf6133ce56a
Evan Bacon <baconbrix@gmail.com>
2020-05-15 18:26:55 -0700

    disable pods on non-darwin machine

--- __tests__/index-test.js
+++ __tests__/index-test.js
@@ -144,8 +144,10 @@ describe('templates', () => {
     // Test that the user was warned about deps
     expect(results.stdout).toMatch(/make sure you have node modules installed/);
     expect(results.stdout).toMatch(/npm install/);
-    expect(results.stdout).toMatch(/make sure you have CocoaPods installed/);
-    expect(results.stdout).toMatch(/npx pod-install/);
+    if (process.platform === 'darwin') {
+      expect(results.stdout).toMatch(/make sure you have CocoaPods installed/);
+      expect(results.stdout).toMatch(/npx pod-install/);
+    }
 
     expect(fileExists(projectName, 'package.json')).toBeTruthy();
     expect(fileExists(projectName, 'App.js')).toBeTruthy();
@@ -166,9 +168,10 @@ describe('templates', () => {
     // Test that the user was warned about deps
     expect(results.stdout).toMatch(/make sure you have node modules installed/);
     expect(results.stdout).toMatch(/yarn/);
-    expect(results.stdout).toMatch(/make sure you have CocoaPods installed/);
-    expect(results.stdout).toMatch(/npx pod-install/);
-
+    if (process.platform === 'darwin') {
+      expect(results.stdout).toMatch(/make sure you have CocoaPods installed/);
+      expect(results.stdout).toMatch(/npx pod-install/);
+    }
     expect(fileExists(projectName, 'package.json')).toBeTruthy();
     expect(fileExists(projectName, 'App.js')).toBeTruthy();
     expect(fileExists(projectName, 'README.md')).toBeTruthy();
