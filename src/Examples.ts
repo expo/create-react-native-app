0350c7b28ed2475f781ea757af925f4e3cc1c961
Douglas Lowder <douglowder@mac.com>
2023-05-11 14:36:41 -0700

    Fix issue with vercel dependency and TS errors

--- src/Examples.ts
+++ src/Examples.ts
@@ -151,6 +151,7 @@ export async function resolveTemplateArgAsync(
       // @ts-ignore
       repoUrl = new URL(template);
     } catch (error) {
+      // @ts-ignore
       if (error.code !== 'ERR_INVALID_URL') {
         oraInstance.fail(error);
         process.exit(1);
