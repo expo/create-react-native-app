83aca280624ea2d7cc116b6cd55f1307394ce175
Cedric van Putten <github@cedric.dev>
2024-06-05 19:04:07 +0200

    refactor: sunset package as outlined in RFC 0759
    see: https://github.com/react-native-community/discussions-and-proposals/blob/main/proposals/0759-react-native-frameworks.md\#the-init-command

---
+++ src/__tests__/index.test.js
@@ -0,0 +1,41 @@
+/* eslint-env jest */
+import execa from 'execa';
+
+const cli = require.resolve('../../build/index.js');
+const execute = () => execa('node', [cli]).catch((error) => error);
+
+it('exits with error code', async () => {
+  expect(await execute()).toMatchObject({ exitCode: 1 });
+});
+
+it('logs deprecation warning', async () => {
+  expect(await execute()).toMatchObject({
+    stderr: expect.stringContaining('This tool does not initialize new React Native projects'),
+  });
+});
+
+it('logs next step', async () => {
+  expect(await execute()).toMatchObject({
+    stderr: expect.stringContaining(
+      `It's recommended to use a framework to build apps with React Native`
+    ),
+  });
+});
+
+it('logs expo as alternative', async () => {
+  expect(await execute()).toMatchObject({
+    stderr: expect.stringContaining(`npx create-expo-app`),
+  });
+});
+
+it('logs @react-native-community/cli as alternative', async () => {
+  expect(await execute()).toMatchObject({
+    stderr: expect.stringContaining(`npx @react-native-community/cli init`),
+  });
+});
+
+it('logs more information link', async () => {
+  expect(await execute()).toMatchObject({
+    stderr: expect.stringContaining('https://reactnative.dev/docs/getting-started'),
+  });
+});
